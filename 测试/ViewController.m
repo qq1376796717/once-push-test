e74e83485a0aa962dd18cb06b7140d3d7ff2ed3c
非凡科技 <feifankeji@feifankejidemac-mini-3.local>
2016-09-03 11:01:45 +0800

    1232

--- 测试/ViewController.m
+++ 测试/ViewController.m
@@ -9,14 +9,40 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+{
+    NSArray *url;
+    NSMutableArray *array_image;
+    
+}
+@property (weak, nonatomic) IBOutlet UIImageView *imageview;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    array_image=[NSMutableArray array];
+    url=@[@"http://www.apoints.com/graphics/UploadFiles/200803/20080301202754140.jpg",@"http://www.apoints.com/graphics/UploadFiles/200803/20080301202754140.jpg",@"http://www.apoints.com/graphics/UploadFiles/200803/20080301202754140.jpg",@"http://www.apoints.com/graphics/UploadFiles/200803/20080301202754140.jpg"];
+    for (int i=0; i<url.count; i++) {
+        NSThread *thread=[[NSThread alloc]initWithTarget:self selector:@selector(loadImage:) object:nil];
+        thread.name=@"thread";
+        
+        [thread start];
+    }
+}
+
+-(void)loadImage:(NSNumber *)number{
+    NSData *data=[NSData dataWithContentsOfURL:[NSURL URLWithString:url[[number intValue]]]];
+    [array_image addObject:data];
+    NSLog(@">>>R%@",data);
+    [self performSelectorOnMainThread:@selector(updataImage:) withObject:data waitUntilDone:YES];
+    
+}
+-(void)updataImage:(NSNumber *)number{
+    UIImage *image_s=array_image [[number intValue]+1 ];
+    self.imageview.image=image_s;
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
